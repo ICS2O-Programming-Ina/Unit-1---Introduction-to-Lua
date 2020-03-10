d074c803c385a16ae50a2d0b626f24ef70a819d4
Ina Tolo <ina.tolo@ocsbstudent.ca>
2020-03-10 14:29:55 -0400

    added comments to Numeric Text Field

--- NumTextFieldsIna/main.lua
+++ NumTextFieldsIna/main.lua
@@ -1,7 +1,9 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: NumericTextFields
+-- Name: Ina 
+-- Course: ICS2O
+-- This program displays a math question and asks the user to answer in a numeric textField
+-- terminal 
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar 
+display.setStatusBar(display.HiddenStatusBar)
